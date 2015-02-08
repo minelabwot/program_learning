9fc64963e0316b29bcf23c7e9f49f37f3dabb5b9
stczwd <qcsd2011@163.com>
2015-02-08 19:04:55 +0800

    c++ leaning

---
+++ c++/myfirst.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: myfirst.cpp
+	> Author:lijunqing
+	> Mail: qcsd2011@163.com 
+	> Created Time: 日  2/ 8 18:57:01 2015
+ ************************************************************************/
+//myfirst.cpp ---> displays a message
+
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout << "Come up and C++ me some time." << endl;
+	cout << "You won't regret it!" << endl;
+	return 0;
+}
